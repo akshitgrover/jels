9a4ed28e75217e4e6be035839ab807d9edfa20a2
Akshit Grover <akshit.grover2016@gmail.com>
2019-05-16 02:02:30 +0530

    Dhandler: Add leaf, node structures

---
+++ lib/dhandler/index.ts
@@ -0,0 +1,16 @@
+// Layout for required fields in the leaf node of B+ Tree.
+interface leaf {
+    fileOffset: number | null;
+    parent: node | null;
+    keys: number[];
+}
+
+/* 
+    Structure for internal nodes of B+ trees.
+    Extends `leaf` to avoid complications for child pointers type.
+    Child pointer, Can point to both leaf and other internal nodes.
+*/
+interface node extends leaf {
+    childPointers: Array<node | null>[];
+    isLeaf: boolean;
+}
