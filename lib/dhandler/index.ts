7febc9e1a51fc1ae075bb63f7a3b2272ce6efd32
Akshit Grover <akshit.grover2016@gmail.com>
2019-05-17 16:59:54 +0530

    Add editorconfig

--- lib/dhandler/index.ts
+++ lib/dhandler/index.ts
@@ -1,33 +1,34 @@
 // Layout for required fields in the leaf node of B+ Tree.
-interface leaf<V, K> {
-    value: V | null;
-    parent: node<V, K> | null;
-    keys: K[];
+interface leaf<K, V> {
+  value: V | null;
+  parent: node<V, K> | null;
+  keys: K[];
 }
 
-/* 
+/*
     Structure for internal nodes of B+ trees.
     Extends `leaf` to avoid complications for child pointers type.
     Child pointer, Can point to both leaf and other internal nodes.
 */
-interface node<V, K> extends leaf<V, K> {
-    childPointers: Array<node<V, K> | null>[];
-    isLeaf: boolean;
+interface node<K, V> extends leaf<K, V> {
+  childPointers: Array<node<K, V> | null>[];
+  isLeaf: boolean;
 }
 
 /*
     Blueprint of B+ Trees
 */
-class tree <V, K> {
-    protected root: node<V, K>;
-    
-    constructor (readonly bfactor: number = 4) {
-        this.root = {
-            isLeaf: false,
-            childPointers: [],
-            value: null,
-            parent: null,
-            keys: [],
-        }
-    };
+class tree <K, V> {
+  protected root: node<K, V>;
+
+  constructor (readonly bfactor: number = 4) {
+    this.root = {
+        isLeaf: false,
+        childPointers: [],
+        value: null,
+        parent: null,
+        keys: [],
+    }
+  };
+
 }
