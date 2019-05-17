360b5e48feeb1843d41716578cf691129710c8d4
Akshit Grover <akshit.grover2016@gmail.com>
2019-05-17 19:34:22 +0530

    Dhandler: Add declaration file

---
+++ lib/dhandler/index.d.ts
@@ -0,0 +1,26 @@
+// Layout for required fields in the leaf node of B+ Tree.
+interface leaf<K, V> {
+  value: V | null;
+  parent: node<K, V> | null;
+  keys: K[];
+}
+
+/*
+    Structure for internal nodes of B+ trees.
+    Extends `leaf` to avoid complications for child pointers type.
+    Child pointer, Can point to both leaf and other internal nodes.
+*/
+interface node<K, V> extends leaf<K, V> {
+  childPointers: Array<node<K, V> | null>;
+  isLeaf: boolean;
+}
+
+/*
+    Blueprint of B+ Trees
+*/
+declare class tree <K, V> {
+  protected root: node<K, V>;
+  readonly bfactor: number;
+
+  insert: (key: K, value: V) => void;
+}
