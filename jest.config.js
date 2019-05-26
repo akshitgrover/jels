ab4e9395f75f0b948d5a308a519addcf685d3f7c
Akshit Grover <akshit.grover2016@gmail.com>
2019-05-26 23:03:43 +0530

    Tests: Configure test environment

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/src/tests"
+  ],
+  "moduleFileExtensions": ["ts", "json", "tsx", "js", "jsx", "node"],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+}
---
+++ src/tests/lib/dhandler.test.ts
@@ -0,0 +1,21 @@
+import Tree from "../../lib/dhandler/tree";
+
+let t: Tree<number>
+
+beforeAll(() => {
+  t = new Tree(2);
+});
+
+test("Btree Traversal", () => {
+  let t: Tree<number> = new Tree(2);
+  t.insert(7, 11);
+  t.insert(8, 11);
+  expect(t.traverse().trim()).toBe("7 | 7,8 |");
+  t.insert(6, 11);
+  t.insert(9, 11);
+  t.insert(10, 11);
+  t.insert(16, 15);
+  t.insert(5, 8);
+  expect(t.traverse().trim()).toBe("8 | 7 | 9,10 | 5,6 | 7 | 8 | 9 | 10,16 |");
+});
+
